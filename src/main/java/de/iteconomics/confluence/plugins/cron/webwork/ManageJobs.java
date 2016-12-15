055f61f58e8ee0f985c1728781d63b9ce0d70a7d
Claudio Kirchhoff <ckirchhoff@it-economics.de>
2016-12-15 11:43:17 +0100

    Create basic admin screen for jobs

---
+++ src/main/java/de/iteconomics/confluence/plugins/cron/webwork/ManageJobs.java
@@ -0,0 +1,17 @@
+package de.iteconomics.confluence.plugins.cron.webwork;
+
+import com.atlassian.confluence.spaces.actions.SpaceAdminAction;
+
+public class ManageJobs extends SpaceAdminAction {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -6410989010347901283L;
+
+	@Override
+	public String doDefault() {
+		return INPUT;
+	}
+
+}
